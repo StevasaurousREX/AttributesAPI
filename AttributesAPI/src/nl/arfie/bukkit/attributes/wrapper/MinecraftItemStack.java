195400b2b9e5ebc0c4130a3ef0fe511280de4c8a
Arfie99 <ruud@arfie.nl>
2014-05-04 14:00:42 +0200

    Initial commit.

---
+++ AttributesAPI/src/nl/arfie/bukkit/attributes/wrapper/MinecraftItemStack.java
@@ -0,0 +1,29 @@
+package nl.arfie.bukkit.attributes.wrapper;
+
+public class MinecraftItemStack extends SourceWrapper {
+
+	private static Class<?> clazz = loadClass("net.minecraft.server","ItemStack");
+	
+	public MinecraftItemStack() throws InstantiationException, IllegalAccessException{super(clazz.newInstance());}
+	public MinecraftItemStack(Object instance){super(instance);}
+	
+	public void setTag(NBTTagCompound tag){
+		try {
+			clazz.getField("tag").set(instance,tag.instance);
+		} catch (IllegalArgumentException | IllegalAccessException
+				| NoSuchFieldException | SecurityException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public NBTTagCompound getTag(){
+		try {
+			return new NBTTagCompound(clazz.getField("tag").get(instance));
+		} catch (IllegalArgumentException | IllegalAccessException
+				| NoSuchFieldException | SecurityException | InstantiationException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+}
---
+++ AttributesAPI/src/nl/arfie/bukkit/attributes/wrapper/SourceWrapper.java
@@ -0,0 +1,56 @@
+package nl.arfie.bukkit.attributes.wrapper;
+
+import org.bukkit.Bukkit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+abstract class SourceWrapper {
+
+	protected Object instance;
+	
+	private static HashMap<String, Method> methods = new HashMap<String, Method>();
+	
+	public SourceWrapper(Object instance){
+		this.instance=instance;
+	}
+	
+	protected static Class<?> loadClass(String start, String end){
+		for(int i=1; i<=25; ++i){
+			try {
+				return Bukkit.class.getClassLoader().loadClass(start+".v1_7_R"+i+"."+end);
+			} catch (ClassNotFoundException ex) {}
+		}
+		return null;
+	}
+	
+	protected static void declareMethod(Class<?> clazz, String name, Class<?>... parameterTypes){
+		try {
+			methods.put(name, clazz.getMethod(name,parameterTypes));
+		} catch (NoSuchMethodException | SecurityException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	protected Object invokeMethod(String name, Object... args){
+		try {
+			return methods.get(name).invoke(instance,args);
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	protected static Object invokeStaticMethod(String name, Object... args){
+		try {
+			return methods.get(name).invoke(null,args);
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+}
