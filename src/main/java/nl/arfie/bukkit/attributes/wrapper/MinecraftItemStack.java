4ba179750851e8b1fdc1efef912953f50ade4320
McDjuady <mcdjuady@googlemail.com>
2015-04-16 11:11:12 +0200

    Formatting, Updated to work with Minecraft 1.8

--- src/main/java/nl/arfie/bukkit/attributes/wrapper/MinecraftItemStack.java
+++ src/main/java/nl/arfie/bukkit/attributes/wrapper/MinecraftItemStack.java
@@ -1,29 +1,48 @@
 package nl.arfie.bukkit.attributes.wrapper;
 
+import java.lang.reflect.Field;
+import org.bukkit.Bukkit;
+
 public class MinecraftItemStack extends SourceWrapper {
 
-	private final static Class<?> clazz = loadClass("net.minecraft.server","ItemStack");
-	
-	public MinecraftItemStack() throws InstantiationException, IllegalAccessException{super(clazz.newInstance());}
-	public MinecraftItemStack(Object instance){super(instance);}
-	
-	public void setTag(NBTTagCompound tag){
-		try {
-			clazz.getField("tag").set(instance,tag.instance);
-		} catch (IllegalArgumentException | IllegalAccessException
-				| NoSuchFieldException | SecurityException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	public NBTTagCompound getTag(){
-		try {
-			return new NBTTagCompound(clazz.getField("tag").get(instance));
-		} catch (IllegalArgumentException | IllegalAccessException
-				| NoSuchFieldException | SecurityException | InstantiationException ex) {
-			ex.printStackTrace();
-			return null;
-		}
-	}
-	
+    private final static Class<?> clazz = loadClass("net.minecraft.server", "ItemStack");
+    private static Field tagField;
+
+    static {
+        try {
+            tagField = clazz.getDeclaredField("tag");
+            tagField.setAccessible(true);
+        } catch (NoSuchFieldException | SecurityException ex) {
+            Bukkit.getLogger().info("Failed to set \"Tag\" accessible!");
+        }
+    }
+
+    public MinecraftItemStack() throws InstantiationException, IllegalAccessException {
+        super(clazz.newInstance());
+    }
+
+    public MinecraftItemStack(Object instance) {
+        super(instance);
+    }
+
+    public void setTag(NBTTagCompound tag) {
+        try {
+            tagField.set(instance, tag.instance);
+        } catch (IllegalArgumentException | IllegalAccessException | SecurityException ex) {
+            Bukkit.getLogger().info(instance.getClass().getName());
+            ex.printStackTrace();
+
+        }
+    }
+
+    public NBTTagCompound getTag() {
+        try {
+            return new NBTTagCompound(tagField.get(instance));
+        } catch (IllegalArgumentException | IllegalAccessException | SecurityException | InstantiationException ex) {
+            Bukkit.getLogger().info(instance.getClass().getName());
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- src/main/java/nl/arfie/bukkit/attributes/wrapper/SourceWrapper.java
+++ src/main/java/nl/arfie/bukkit/attributes/wrapper/SourceWrapper.java
@@ -8,55 +8,53 @@ import java.util.HashMap;
 
 abstract class SourceWrapper {
 
-	protected Object instance;
-	
-	private static final HashMap<String, Method> methods = new HashMap<String, Method>();
-	
-	protected static String v;
-	
-	static{
-		String pkgName = Bukkit.getServer().getClass().getPackage().getName();
-		v = "."+pkgName.substring(pkgName.lastIndexOf('.')+1)+".";
-	}
-	
-	public SourceWrapper(Object instance){
-		this.instance=instance;
-	}
-	
-	protected static Class<?> loadClass(String start, String end){
-		try {
-			return Bukkit.class.getClassLoader().loadClass(start+v+end);
-		} catch (ClassNotFoundException ex) {
-			return null;
-		}
-	}
-	
-	protected static void declareMethod(Class<?> clazz, String name, Class<?>... parameterTypes){
-		try {
-			methods.put(name, clazz.getMethod(name,parameterTypes));
-		} catch (NoSuchMethodException | SecurityException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	protected Object invokeMethod(String name, Object... args){
-		try {
-			return methods.get(name).invoke(instance,args);
-		} catch (IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException ex) {
-			ex.printStackTrace();
-			return null;
-		}
-	}
-	
-	protected static Object invokeStaticMethod(String name, Object... args){
-		try {
-			return methods.get(name).invoke(null,args);
-		} catch (IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException ex) {
-			ex.printStackTrace();
-			return null;
-		}
-	}
-	
+    protected Object instance;
+
+    private static final HashMap<String, Method> methods = new HashMap<>();
+
+    protected static String v;
+
+    static {
+        String pkgName = Bukkit.getServer().getClass().getPackage().getName();
+        v = "." + pkgName.substring(pkgName.lastIndexOf('.') + 1) + ".";
+    }
+
+    public SourceWrapper(Object instance) {
+        this.instance = instance;
+    }
+
+    protected static Class<?> loadClass(String start, String end) {
+        try {
+            return Bukkit.class.getClassLoader().loadClass(start + v + end);
+        } catch (ClassNotFoundException ex) {
+            return null;
+        }
+    }
+
+    protected static void declareMethod(Class<?> clazz, String name, Class<?>... parameterTypes) {
+        try {
+            methods.put(name, clazz.getMethod(name, parameterTypes));
+        } catch (NoSuchMethodException | SecurityException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    protected Object invokeMethod(String name, Object... args) {
+        try {
+            return methods.get(name).invoke(instance, args);
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    protected static Object invokeStaticMethod(String name, Object... args) {
+        try {
+            return methods.get(name).invoke(null, args);
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
 }
